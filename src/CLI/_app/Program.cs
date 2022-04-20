f024724e142c87dff238a991fa6ab109d29917e0
DarkCreekWay <47448914+DarkCreekWay@users.noreply.github.com>
2022-04-20 20:52:55 +0200

    Add simplistic argument routing for the 'capture' command

--- src/CLI/_app/Program.cs
+++ src/CLI/_app/Program.cs
@@ -1,6 +1,35 @@
 ﻿namespace DarkCreekWay.FileStructures.CLI {
 
     static class Program {
-        public static void Main( string[] argv ) { }
+
+        public static void Main( string[] argv ) {
+
+            if( argv.Length == 0 ) {
+                Environment.Exit( 1 );
+            }
+
+            if( argv[0].ToLowerInvariant() == "capture" ) {
+
+                if( argv.Length < 2 ) {
+                    Environment.Exit( 1 );
+                }
+
+                Capture( argv[1] );
+
+            }
+
+        }
+
+        static void Capture( string rootPath ) {
+
+            if( false == Directory.Exists( rootPath ) ) {
+                Environment.Exit( 1 );
+            }
+
+            Console.WriteLine( $"Capturing : {rootPath}" );
+            Console.ReadLine();
+
+            throw new NotImplementedException();
+        }
     }
 }
