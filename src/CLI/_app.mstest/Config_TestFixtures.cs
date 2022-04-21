0da0ea0fa881da2a45f7dd02434a74dbdbb3c75e
DarkCreekWay <47448914+DarkCreekWay@users.noreply.github.com>
2022-04-21 17:21:31 +0200

    Add testcase for designing config directory structure

---
+++ src/CLI/_app.mstest/Config_TestFixtures.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DarkCreekWay.FileStructures.CLI {
+
+    [TestClass]
+    public class Config_TestFixtures {
+
+        const string s_CompanyDirectoryName = "DarkCreekWay";
+        const string s_ProductDirectoryName = "FileStructures";
+        const string s_CapturedStructuresDirectoryName = "Captured";
+        const string s_DefaultCapturedStructuresFilename = ".default";
+
+        [TestMethod]
+        public void BuildUserSpecificConfigPathes() {
+
+            string userConfigBasePath = Environment.GetFolderPath( Environment.SpecialFolder.LocalApplicationData );
+
+            string appUserConfigBasePath = Path.Combine( userConfigBasePath, s_CompanyDirectoryName, s_ProductDirectoryName );
+            string capturedStructuresBasePath = Path.Combine( appUserConfigBasePath, s_CapturedStructuresDirectoryName );
+            string capturedStructuresDefaultPath = Path.Combine( capturedStructuresBasePath, s_DefaultCapturedStructuresFilename );
+
+            Debug.WriteLine( $"User Config Base Path            (D) : {userConfigBasePath}" );
+            Debug.WriteLine( $"App User Config Base Path        (D) : {appUserConfigBasePath}" );
+            Debug.WriteLine( $"Captured Structures Base Path    (D) : {capturedStructuresBasePath}" );
+            Debug.WriteLine( $"Captured Structures Default Path (F) : {capturedStructuresDefaultPath}" );
+
+        }
+    }
+}
