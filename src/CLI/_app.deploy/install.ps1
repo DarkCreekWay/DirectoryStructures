8c9286ef0a269468628d504b340ff2834f52a5cf
DarkCreekWay <47448914+DarkCreekWay@users.noreply.github.com>
2022-05-24 15:06:37 +0200

    Fix PS install deployment skript

--- src/CLI/_app.deploy/install.ps1
+++ src/CLI/_app.deploy/install.ps1
@@ -6,18 +6,17 @@
   current user's profile
 
   .Description
-  DarkCreekWay's tool for Directory Strucures gets deployed by coping all 
-  required files to the following directory:
+  DarkCreekWay's tool for Directory Strucures gets deployed by coping
+  all required files to the following directory:
   %localappdata%\Programs\DarkCreekWay\DirectoryStructurs 
 
   For finishing the deployment, run ds.xonfig.exe
 #>
 
-# $sourceDirectory = 'p:\DirectoryStructures\src\CLI\.Build\artefacts\net6.0-windows\*'
 $sourceDirectory = $PSScriptRoot
 $targetDirectory = Join-Path -Path $env:LOCALAPPDATA -ChildPath 'Programs\DarkCreekWay\DirectoryStructures'
 
 New-Item $targetDirectory -ItemType Directory -Force | Out-Null
-Copy-Item -Path $sourceDirectory -Destination $targetDirectory -Recurse -Force
+Copy-Item -Path "$sourceDirectory\*" -Destination $targetDirectory -Recurse -Force
 
 Start-Process (Join-Path -Path $targetDirectory -ChildPath 'ds.config.exe')
